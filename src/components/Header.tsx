2282c1a782392a3af3417b0d0e567c20f78bbc94
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-02-03 21:28:23 -0500

    added new pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import icon from "../assets/images/bmi-icon.png";
 function Header() {
   return (
-    <div className=" bg-accent text-slate-50 flex w-full">
+    <div className=" bg-accent text-slate-50 flex w-full border-2 border-green-500 justify-between">
       <div>
-        <h1>BMI Calculator</h1>
+        <h1 className=" w-1/2">BMI Calculator</h1>
         <h2>use this calculator to check your body mass index(BMI)</h2>
       </div>
-      <img src={icon} />
+      <img className=" w-1/2" src={icon} />
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import Header from "../components/Header";
+import Form from "../components/Form";
+import Footer from "../components/Footer";
 function Home() {
   return (
-    <div className="w-full h-full flex">
+    <div className="w-full h-full flex border-2 border-red-700 flex-col">
       <Header />
+      <Form />
+      <Footer />
     </div>
   );
 }
